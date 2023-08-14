28b7b75b6380ee4740c48f4681874e26cc04969d
Kazuho1222 <destiny5914@gmail.com>
2023-08-15 08:08:50 +0900

    エラーメッセージの日本語化対応

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,5 +14,18 @@ class Event < ApplicationRecord
   validates :title, :description, :category_id, :meeting_place, :return_place, :distance, :climbing, :start_time, :end_time, :capacity, :user, :images,
             presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :capacity, presence: true, numericality: { greater_than: 0 }
+  validates :capacity, :distance, :climbing, presence: true, numericality: { greater_than: 0 }
+  validate :date_before_start
+  validate :date_before_end
+
+  def date_before_start
+    return if start_time.blank?
+    errors.add(:start_time, "は今日以降のものを選択してください") if start_time < Time.zone.today
+  end
+
+  def date_before_end
+    return if end_time.blank? || start_time.blank?
+    errors.add(:end_time, "は開始日以降のものを選択してください") if end_time < start_time
+  end
+
 end
