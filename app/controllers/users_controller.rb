4712430945a3c54922ff1dbd073fa874251e568d
Kazuho1222 <destiny5914@gmail.com>
2023-08-16 17:02:27 +0900

    ユーザー情報編集でログイン状態維持の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       redirect_to @user, success: 'プロフィールを変更しました。'
+      bypass_sign_in(@user)
     else
       render :edit
     end
