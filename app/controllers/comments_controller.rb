ce10d5be9309e8c3614a4d7819f321bada6bb344
Kazuho1222 <destiny5914@gmail.com>
2023-08-17 20:10:10 +0900

    rubocop実行

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
 class CommentsController < ApplicationController
-
   def create
     @comment = Comment.new(comment_params)
     @event = Event.find(params[:event_id])
-    if @comment.save
-      CommentChannel.broadcast_to @event, {comment: @comment, user: @comment.user}
-    end
+    return unless @comment.save
+
+    CommentChannel.broadcast_to @event, { comment: @comment, user: @comment.user }
   end
 
   private
@@ -13,5 +12,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, event_id: params[:event_id])
   end
-
 end
